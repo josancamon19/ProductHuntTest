9e0b551188aa2b40eb456d07467a703b491b7bde
Joan <joan.santiago.cabezas@gmail.com>
2021-10-19 11:50:58 +0200

    Profilefragment UI set with user real data

--- app/src/main/java/com/josancamon19/producthunttest/views/profile/ProfileFragment.kt
+++ app/src/main/java/com/josancamon19/producthunttest/views/profile/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.josancamon19.producthunttest.views.profile
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,13 +9,10 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.apollographql.apollo.coroutines.await
-import com.josancamon19.producthunttest.PostDetailsQuery
+import com.bumptech.glide.Glide
 import com.josancamon19.producthunttest.UserDetailsQuery
-import com.josancamon19.producthunttest.databinding.FragmentDetailBinding
 import com.josancamon19.producthunttest.databinding.FragmentProfileBinding
 import com.josancamon19.producthunttest.network.apolloClient
-import com.josancamon19.producthunttest.views.detail.DetailFragmentArgs
-import timber.log.Timber
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
@@ -28,7 +26,25 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         lifecycleScope.launchWhenResumed {
             val response = apolloClient().query(UserDetailsQuery(args.userId)).await()
+            response.data?.user?.let { setupViews(it) }
         }
         return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setupViews(user: UserDetailsQuery.User) {
+        Glide.with(binding.root)
+            .load(user.coverImage).centerCrop()
+            .into(binding.ivProfileBanner)
+
+        Glide.with(binding.root)
+            .load(user.profileImage).circleCrop()
+            .into(binding.ivProfileImage)
+
+        binding.tvProfileName.text=  user.name
+        if (user.headline.isNullOrEmpty()){
+            binding.tvProfileHeadline.visibility = View.GONE
+        }else binding.tvProfileHeadline.text = user.headline
+        binding.tvProfileId.text = "#${user.id} ${user.username} ${user.headline}"
     }
 }
